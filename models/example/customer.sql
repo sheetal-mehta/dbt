012ea3b6a1b43d3d258e9badf36597d3a7ae2d9b
sheetal.mehta@stud.hochschule-heidelberg.de <sheetal.mehta@stud.hochschule-heidelberg.de>
2023-11-03 12:00:55 +0000

    updated customers

--- models/example/customer.sql
+++ models/example/customer.sql
@@ -1,37 +1,11 @@
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
-
+    select * from {{ref("stg_customer")}}
 ),
 
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
-
-),
 
 customer_orders as (
 
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
+    select * from {{ref("stg_customerorder")}}
 
 ),
 
